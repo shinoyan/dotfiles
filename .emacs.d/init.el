39990909097e6ae010632045728befaa827067b1
shinoyan <taste.of.emo@gmail.com>
2019-01-24 14:33:20 +0900

    Slackの設定をプライベイトディレクトリからロードする様に対応
    - dropboxのディレクトリをinit.elで定義する様に対応

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,12 +24,15 @@
 
 ;;; init.orgをコンパイル
 (use-package org
-     :straight t)
-(defvar my-config-dir (concat user-emacs-directory "conf.d/"))
+  :straight t)
+(defvar dropbox-dir "~/Dropbox/")
+(defvar my-config-dir (concat (file-name-as-directory user-emacs-directory) "conf.d/"))
+;; dotファイルで管理しない様な設定の置き場所
+(defvar private-config-dir (concat (file-name-as-directory dropbox-dir) "emacs.d/"))
 (org-babel-load-file (expand-file-name "init.org" my-config-dir))
 ;;; slack.orgが存在する場合はコンパイル
-(if (file-exists-p (expand-file-name "slack.org" my-config-dir))
+(when (and (file-exists-p (expand-file-name "slack.org" my-config-dir))
+           (file-exists-p (expand-file-name "slack-config.el" private-config-dir)))
   (org-babel-load-file (expand-file-name "slack.org" my-config-dir)))
-
 (provide 'init)
 ;;; init.el ends here
